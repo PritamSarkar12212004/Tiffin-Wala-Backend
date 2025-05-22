cbd4bd88a6da2e8087918572f36cadc6aa318e35
Pritam Sarkar <you@example.com>
2025-05-22 19:08:49 +0530

    change sone formate

--- src/controller/user/profileCreateController.js
+++ src/controller/user/profileCreateController.js
@@ -6,9 +6,8 @@ const profileCreateController = async (req, res) => {
     const fullAddress = {
       latitude: location.latitude,
       longitude: location.longitude,
-      address: location.formattedAddress,
+      address: location.address,
     };
-    console.log(fullAddress);
     const findData = await userModel.findOne({ User_Phone_Number: phone });
     if (findData) {
       return res.status(400).json({
@@ -16,21 +15,20 @@ const profileCreateController = async (req, res) => {
         message: "User already exists",
       });
     }
-      const userData = await userModel.create({
-        User_Name: username ? username : null,
-        User_Email: email ? email : null,
-        User_Gender: gender ? gender : null,
-        User_Bio: bio ? bio : null,
-        User_Phone_Number: phone ? phone : null,
-        User_Image: image ? image : null,
-        User_Address: fullAddress ? fullAddress : null,
-      });
-      res.status(200).json({
-        success: true,
-        message: "Profile created successfully",
-        data: userData,
-      });
-      
+    const userData = await userModel.create({
+      User_Name: username ? username : null,
+      User_Email: email ? email : null,
+      User_Gender: gender ? gender : null,
+      User_Bio: bio ? bio : null,
+      User_Phone_Number: phone ? phone : null,
+      User_Image: image ? image : null,
+      User_Address: fullAddress ? fullAddress : null,
+    });
+    res.status(200).json({
+      success: true,
+      message: "Profile created successfully",
+      data: userData,
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({
